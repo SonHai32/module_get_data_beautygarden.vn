2ef66e1476948a0b5257efb62b60f58685c548d7
root <lamsonhai1379@gmail.com>
2021-05-11 07:14:13 +0000

    add try-except

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import time
 import json
 driver = webdriver.Chrome()
 
-driver.get('https://beautygarden.vn/danh-muc/kem-nen.html')
+driver.get('https://beautygarden.vn/danh-muc/sua-rua-mat.html?page=2')
 
 time.sleep(2)
 driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div/div/button[1]').click()
@@ -23,31 +23,34 @@ def get_data_from_page(page_url):
     driver.get(page_url)
 
 
-    prd_cat = {
-        "cat_id": "kem-nem", 
-        "cat_text": "Kem nền", 
-        "parent_cat": {
-            "cat_id": "trang-diem", 
-            "cat_text": "Trang Điểm"}}
-    prd_name = driver.find_element_by_class_name('title-Product').text
-    prd_price = driver.find_element_by_class_name('price-drop').find_elements_by_tag_name('span')[0].text
-    prd_old_price = driver.find_element_by_class_name('price-vince').find_elements_by_tag_name('span')[0].text
-    prd_discount = driver.find_element_by_class_name('price-vince').find_elements_by_tag_name('span')[2].text
-    prd_rating = driver.find_element_by_class_name('text-rating').find_elements_by_tag_name('span')[0].text
-    prd_color = []
-    prd_img_url = []
-    
-    img_container = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[1]/div[1]/div/div[1]/div/div[2]/div/div[1]').find_elements_by_tag_name('a')
-    for item_img in img_container:
-        prd_img_url.append(item_img.get_attribute('href'))
-
     try:
-        color_select_items = driver.find_element_by_class_name('select-land').find_elements_by_tag_name('li')
-        for item in color_select_items:
-            item_color_hex = item.find_element_by_tag_name('a').get_attribute('style')
-            prd_color.append(item_color_hex.split(':')[1].split(';')[0])
-    except:
-        print()
+        prd_cat = {
+            "cat_id": "sua-rua-mat", 
+            "cat_text": "Sữa rửa mặt", 
+            "parent_cat": {
+                "cat_id": "cham-soc-da", 
+                "cat_text": "Chăm Sóc Da"}}
+        prd_name = driver.find_element_by_class_name('title-Product').text
+        prd_price = driver.find_element_by_class_name('price-drop').find_elements_by_tag_name('span')[0].text
+        prd_old_price = driver.find_element_by_class_name('price-vince').find_elements_by_tag_name('span')[0].text
+        prd_discount = driver.find_element_by_class_name('price-vince').find_elements_by_tag_name('span')[2].text
+        prd_rating = driver.find_element_by_class_name('text-rating').find_elements_by_tag_name('span')[0].text
+        prd_color = []
+        prd_img_url = []
+        
+        img_container = driver.find_element_by_xpath('/html/body/main/div[2]/div/div/div[1]/div[1]/div/div[1]/div/div[2]/div/div[1]').find_elements_by_tag_name('a')
+        for item_img in img_container:
+            prd_img_url.append(item_img.get_attribute('href'))
+
+        try:
+            color_select_items = driver.find_element_by_class_name('select-land').find_elements_by_tag_name('li')
+            for item in color_select_items:
+                item_color_hex = item.find_element_by_tag_name('a').get_attribute('style')
+                prd_color.append(item_color_hex.split(':')[1].split(';')[0])
+        except:
+            print()
+    except: 
+        return
 
     
 
